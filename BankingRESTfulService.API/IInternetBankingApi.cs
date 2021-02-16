2451377ae6092fdd3663472f3e97e7ba20124dce
Niren Patel <nirenmpatel@gmail.com>
2021-02-16 18:36:57 +0200

    Add files via upload

---
+++ BankingRESTfulService.API/IInternetBankingApi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankingRESTfulService.API
+{
+    public interface IInternetBankingApi
+    {
+        string GetApiVersion();
+        string CalculateMD5(string value);
+        bool IsPasswordStrong(string password);
+    }
+}
